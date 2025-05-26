3ae477edddcddd022d4ef544300e9c6c13866e4f
Taua Carvalho <tauabruno@gmail.com>
2025-05-26 16:37:36 -0300

    new exercises

--- fundamentos/secao-03-javascript/03exercicios-teste-seu-conhecimento.js
+++ fundamentos/secao-03-javascript/03exercicios-teste-seu-conhecimento.js
@@ -48,3 +48,59 @@ function lIndex (iNumber) {
     return placeHolder;
 }
 console.log(lIndex(numeros));
+
+console.log('');
+console.log('Exercício 4');
+console.log('');
+
+const words = ['apple', 'pineapple', 'garlics', 'pine', 'grape'];
+
+function sWord (wNumber) {
+    let bSize = 0;
+    let bWord
+    for (index = 0; index < wNumber.length; index += 1) {
+        if (wNumber[index].length > bSize) {
+            bSize = wNumber[index].length;
+            bWord = wNumber[index];    
+        }
+    }
+    return bWord;
+}
+console.log(sWord(words));
+
+
+console.log('');
+console.log('Exercício 5');
+console.log('');
+
+const numbers = [7, 3, 7, 2, 4, 3, 7, 1, 2, 7];
+
+let repNumber = 0;
+const contagems = {};
+let mostFrequent = '';
+let number = 0;
+
+function rNumber (repet) {
+    for (index = 0; index < repet.length; index += 1) {
+      number = repet[index];   
+      if (undefined === contagems[number]) {
+        contagems[number] = 1;
+        } else {
+            contagems[number] += 1;
+        }
+    } 
+     
+    
+    for (let number in contagems) {
+        if (contagems[number] > repNumber) {
+            repNumber = contagems[number];
+            mostFrequent = Number(number);
+        }
+    }
+    return mostFrequent
+}
+
+
+console.log(rNumber(numbers));
+
+
