75ab2d5f528517cf6748174b10706af2203523af
Taua Carvalho <tauabruno@gmail.com>
2025-06-19 15:11:52 -0300

    exercícios de DOM

---
+++ fundamentos/secao-04-dom-eventos-e-webstorage/02-criando-elementos-exercicios.js
@@ -0,0 +1,39 @@
+let irmao = document.createElement('section');
+
+irmao.id = "irmao";
+
+parent = document.getElementById("pai");
+
+parent.appendChild(irmao);
+
+console.log("irmao criado", irmao);
+
+let filhoOndeVoceEsta = document.createElement("section");
+
+filhoOndeVoceEsta.id = "filho de ElementoOndeVoceEsta";
+
+parent1 = document.getElementById("elementoOndeVoceEsta");
+
+parent1.appendChild(filhoOndeVoceEsta);
+
+console.log("FilhoOndeVoceEsta criado", filhoOndeVoceEsta);
+
+let filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
+
+filhoDoPrimeiroFilhoDoFilho.id = "filhoDoPrimeiroFilhoDoFilho";
+
+parent2 = document.getElementById("primeiroFilhoDoFilho");
+
+parent2.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+console.log("filhoDoPrimeiroFilhoDoFilho criado", filhoDoPrimeiroFilhoDoFilho);
+
+let irmao1 = document.getElementById("filhoDoPrimeiroFilhoDoFilho");
+
+pai = irmao1.parentElement.parentElement;
+
+let terceiroFilho = pai.nextElementSibling;
+
+console.log(terceiroFilho);
+
+
